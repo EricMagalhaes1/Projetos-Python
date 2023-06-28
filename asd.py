73fae49a5f75b3a344b56021e9681b23ec2346d1
EricMagalhaes1 <kir3vv@gmail.com>
2023-06-27 23:05:31 -0300

    Update

---
+++ asd.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+
+
+dbc.Button(id='botao_avatar',
+                    children=[html.Img(src="/assets/img_hom.png", id="avatar_change", alt="Avatar", className='perfil_avatar'),
+                ], style={'background-color': 'transparent', 'border-color': 'transparent'}),
