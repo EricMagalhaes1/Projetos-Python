e3311c3835fce058cbfb325905beeaf2674a5f37
Eric Magalhaes <kir3vv@gmail.com>
2023-08-03 16:14:55 -0300

    Update

--- Gerador QRCode/main.py
+++ Gerador QRCode/main.py
@@ -2,4 +2,4 @@ from pyqrcode import *
 from png import *
 url = pyqrcode.create('https://www.instagram.com/lnleric/', error='L', version=4)
 url.svg('ig-url.svg', scale=5)
-
+url.png('ig-url.png', scale=5, module_color=(127,17,244), background=(255,255,255))
