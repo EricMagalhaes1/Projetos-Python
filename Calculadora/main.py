806c2a4d8de4d73f50d3a68969865913611fe216
Eric Magalhaes <kir3vv@gmail.com>
2023-08-04 16:34:27 -0300

    Update main.py

--- Calculadora/main.py
+++ Calculadora/main.py
@@ -3,12 +3,32 @@ import tkinter as tk
 calculo = ""
 
 def add_calculo(sylbol):
-    pass
+    global calculo
+    calculo += str(sylbol)
+    texto_resultado.delete(1.0, "end")
+    texto_resultado.insert(1.0, calculo)
 
 def evaluate_calculo():
-    pass
+     global calculo
+     try:
+        print(calculo)
+        calculo = str(eval(calculo))
+        texto_resultado.delete(1.0, "end")
+        texto_resultado.insert(1.0, calculo)
+     except:
+        limpa_campo()
+        texto_resultado.insert(1.0, "Erro")
 
 def limpa_campo():
-    pass
+    global calculo
+    calculo = ""
+    texto_resultado.delete(1.0, "end")
+
+
+root = tk.Tk()
+root.geometry('300x275')
+root.maxsize(300, 275)
+
+
 
 
