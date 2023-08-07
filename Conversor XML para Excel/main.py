d631fc3f8bf5cff876b0513e4214063f131d0db4
Eric Magalhaes <kir3vv@gmail.com>
2023-08-07 14:13:52 -0300

    Upload

---
+++ Conversor XML para Excel/main.py
@@ -0,0 +1,14 @@
+import xmltodict as xd
+import os
+
+def pegar_info(nome_arquivo):
+    print(f'Pegou as Informacoes {nome_arquivo}')
+    with open (f'nfs/{nome_arquivo}', 'rb') as arquivo_xml:
+        dic_arquivo = xd.parse(arquivo_xml)
+
+
+listar_arquivos = os.listdir
+
+for arquivos in listar_arquivos:
+    pegar_info(arquivos)
+    break
