b090965222d12b4a504df1bc05b115d401288575
Eric Magalhaes <kir3vv@gmail.com>
2023-08-12 03:16:16 -0300

    Update main.py

--- Conversor XML para Excel/main.py
+++ Conversor XML para Excel/main.py
@@ -1,11 +1,12 @@
 import xmltodict as xd
 import os
+import json
 
 def pegar_info(nome_arquivo):
     print(f'Pegou as Informacoes {nome_arquivo}')
     with open (f'nfs/{nome_arquivo}', 'rb') as arquivo_xml:
         dic_arquivo = xd.parse(arquivo_xml)
-        print(dic_arquivo)
+        print(json.dumps(dic_arquivo, indent=4))
 
 listar_arquivos = os.listdir("nsf")
 
